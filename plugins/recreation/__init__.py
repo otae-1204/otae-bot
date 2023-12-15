6f7773f1c77a7375f297165f8f87b2156d7c1291
HanazanoTae <2461673400@qq.com>
2023-12-15 11:00:26 +0800

    添加新内容

--- plugins/recreation/__init__.py
+++ plugins/recreation/__init__.py
@@ -6,15 +6,18 @@ from configs.path_config import JSON_PATH, IMAGE_PATH
 from utils.message_builder import at, image
 from nonebot.adapters.onebot.v11 import Message
 from nonebot.adapters.onebot.v11.exception import ActionFailed
+from nonebot.permission import SUPERUSER
+from nonebot.adapters.onebot.v11.permission import GROUP_ADMIN, GROUP_OWNER
 
 MarryGroup = on_regex(r"^/娶群友$|^/jrlp$", flags=re.I)
 MarryGroupByForce = on_regex(r"^/强娶(.+?)$")
 Divorce = on_regex(r"^/离婚$")
+ManageDivorceNum = on_regex(r"^/修改离婚次数(.+?)$", permission=GROUP_ADMIN | GROUP_OWNER | SUPERUSER)
+TodaysLuck = on_regex(r"^/今日人品$|^/jrrp$|^签到$", flags=re.I)
 
 path = JSON_PATH + "recreation/"
 imgPath = IMAGE_PATH + "recreation/"
 updatableNum = 1
-
 
 @MarryGroup.handle()
 async def handle_first_receive(bot: Bot, event: GroupMessageEvent):
@@ -48,11 +51,11 @@ async def handle_first_receive(bot: Bot, event: GroupMessageEvent):
             marryList[groupId][playerId]["partnerId"] = None
             await MarryGroup.finish(Message(f"[CQ:reply,id={msgid}]") + "这个人不在群里了!")
     if playerState == "未娶":
-        partnerId = random.choice(groupMembers)["user_id"]
+        partnerId = str(random.choice(groupMembers)["user_id"])
         while True:
             state = checkPlayerState(groupId, partnerId, date, marryList)
             if state == "已被娶" or partnerId == playerId or state == "已娶":
-                partnerId = random.choice(groupMembers)["user_id"]
+                partnerId = str(random.choice(groupMembers)["user_id"])
             else:
                 break        
         marryList[groupId][playerId]["state"] = 1
@@ -73,7 +76,6 @@ async def handle_first_receive(bot: Bot, event: GroupMessageEvent):
         return
     else:
         await MarryGroup.finish(Message(f"[CQ:reply,id={msgid}]") + "未知错误!")
-
 
 @MarryGroupByForce.handle()
 async def handle_first_receive(bot: Bot, event: GroupMessageEvent):
@@ -138,7 +140,6 @@ async def handle_first_receive(bot: Bot, event: GroupMessageEvent):
     else:
         await MarryGroupByForce.finish(Message(f"[CQ:reply,id={msgId}]") + "未知错误!")        
 
-
 @Divorce.handle()
 async def handle_first_receive(bot: Bot, event: GroupMessageEvent):
     msgId = event.get_event_description().split(" ")[1]
@@ -147,12 +148,13 @@ async def handle_first_receive(bot: Bot, event: GroupMessageEvent):
     date = datetime.date.today().strftime("%Y-%m-%d")
     marryList = read_json(f"{path}MarryList.json")
     playerState = checkPlayerState(groupId, playerId, date, marryList)
+    divorceNum = checkGroupDivorceNum(groupId)
     if playerState == "已娶" or playerState == "已被娶":
         if marryList[groupId][playerId]["updateTime"] == date:
             if marryList[groupId][playerId]["updatableNum"] == 0:
                 await Divorce.finish(Message(f"[CQ:reply,id={msgId}]") + "你今天的离婚次数已经用完了!")
         else:
-            marryList[groupId][playerId]["updatableNum"] = updatableNum
+            marryList[groupId][playerId]["updatableNum"] = divorceNum
         partnerId = str(marryList[groupId][playerId]["partnerId"])
         marryList[groupId][playerId]["state"] = 0
         marryList[groupId][playerId]["updateTime"] = date
@@ -165,6 +167,30 @@ async def handle_first_receive(bot: Bot, event: GroupMessageEvent):
         await Divorce.finish(Message(f"[CQ:reply,id={msgId}]") + f"你成功离婚了,你的离婚次数还有{marryList[groupId][playerId]['updatableNum']}次")
     else:
         await Divorce.finish(Message(f"[CQ:reply,id={msgId}]") + "你今天还没结婚呢!")
+
+@ManageDivorceNum.handle()
+async def handle_first_receive(bot: Bot, event: GroupMessageEvent):
+    msgId = event.get_event_description().split(" ")[1]
+    groupId = event.get_session_id().split("_")[1]
+    DivorceNums = read_json(f"{path}DivorceNums.json")
+    originNum = checkGroupDivorceNum(groupId)
+    try:
+        num = int(re.findall(pattern=r"/修改离婚次数\s+(\S+)",string=str(event.get_message()))[0])
+        if num < 0:
+            await ManageDivorceNum.finish(Message(f"[CQ:reply,id={msgId}]") + "离婚次数不能小于0!")
+        elif num > 20:
+            await ManageDivorceNum.finish(Message(f"[CQ:reply,id={msgId}]") + "离婚次数不能大于20!")
+        elif num == originNum:
+            await ManageDivorceNum.finish(Message(f"[CQ:reply,id={msgId}]") + "离婚次数没有改变!")
+        DivorceNums[groupId] = num
+        write_json(f"{path}DivorceNums.json", DivorceNums)
+        await ManageDivorceNum.finish(Message(f"[CQ:reply,id={msgId}]") + f"成功修改离婚次数为{num}")
+    except TypeError:
+        await ManageDivorceNum.finish(Message(f"[CQ:reply,id={msgId}]") + "输入的内容有误")
+    except Exception as e:
+        await ManageDivorceNum.finish(Message(f"[CQ:reply,id={msgId}]") + f"未知错误:{e}")
+
+
 
 # 检查某人今天的状态
 def checkPlayerState(groupId, playerId, date, marryList) -> str:
@@ -182,29 +208,44 @@ def checkPlayerState(groupId, playerId, date, marryList) -> str:
                     "state" : 0,
                     "updateTime": date,
                     "partnerId": None,
-                    "updatableNum": updatableNum 
+                    "updatableNum": checkGroupDivorceNum(groupId)
                 }
             }
         return "未娶"
-    if playerId in marryList[groupId]:
+    if str(playerId) in marryList[groupId]:
         if marryList[groupId][playerId]["updateTime"] == date and marryList[groupId][playerId]["state"] == 1:
             return "已娶"
-        elif marryList[groupId][playerId]["updateTime"] == date and marryList[groupId][playerId]["partnerId"] != None and marryList[groupId][playerId]["state"] == 2:
+        elif marryList[groupId][playerId]["updateTime"] == date and marryList[groupId][playerId]["state"] == 2:
             return "已被娶"
         else:
             if marryList[groupId][playerId]["updateTime"] != date:
                 marryList[groupId][playerId]["state"] = 0
-                marryList[groupId][playerId]["updatableNum"] = updatableNum
+                marryList[groupId][playerId]["updatableNum"] = checkGroupDivorceNum(groupId)
             return "未娶"
     else:
         marryList[groupId][playerId] = {
             "state" : 0,
             "updateTime": date,
             "partnerId": None,
-            "updatableNum": updatableNum
+            "updatableNum": checkGroupDivorceNum(groupId)
         }
         return "未娶"
 
+# 检查本群离婚次数
+def checkGroupDivorceNum(groupId) -> int:
+    """
+    检查本群离婚次数
+    :param groupId: 群号
+    
+    :return: 离婚次数
+    """
+    DivorceNums = read_json(f"{path}DivorceNums.json")
+    if groupId not in DivorceNums.keys():
+        DivorceNums[groupId] = updatableNum
+        write_json(f"{path}DivorceNums.json", DivorceNums)
+        return updatableNum
+    else:
+        return int(DivorceNums[groupId])
 
 # 读取 JSON 文件
 def read_json(file_path, create_if_not_exists=True):
@@ -246,6 +287,11 @@ def write_json(file_path, data):
 
 # 插件被加载的时候检查资源文件夹有没有被创建
 if not os.path.exists(path):
-    os.makedirs(path) 
+    os.makedirs(path)
+if not os.path.exists(f"{path}DivorceNums.json"):
+    write_json(f"{path}DivorceNums.json", {})
+    print("DivorceNums.json created successfully.")
+if not os.path.exists(f"{path}MarryList.json"):
     write_json(f"{path}MarryList.json", {})
+    print("MarryList.json created successfully.")
 os.makedirs(imgPath) if not os.path.exists(imgPath) else ...
