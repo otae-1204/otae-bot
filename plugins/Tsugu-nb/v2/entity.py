6842ec5c7aae53bf96f2e27d0714139bc8c09fec
HanazanoTae <2461673400@qq.com>
2024-04-24 11:25:58 +0800

    添加User类

--- plugins/Tsugu-nb/v2/entity.py
+++ plugins/Tsugu-nb/v2/entity.py
@@ -2,6 +2,7 @@
 from pathlib import Path
 import json
 
+
 class Config:
     api_base: str                       # 后端API地址
     use_easy_bg: bool                   # 是否使用简易背景
@@ -14,14 +15,13 @@ class Config:
     submit_car_number_use_proxy: bool   # 提交车牌号是否使用代理
     proxy_url: str                      # 代理地址
     server_list: list                   # 服务器列表
-    
-    ban_gacha_simulate_group_data: list # 禁止抽卡模拟的群
+
+    ban_gacha_simulate_group_data: list  # 禁止抽卡模拟的群
     enable_carNum_prompt_groups: list   # 开启车牌上传提示的群或人
-    
+
     cmd_list: list                      # 指令列表
-    
     car_config: dict                    # 车牌配置
-    
+
     def __init__(self):
         configJson = openJson(f"{Path(__file__).parent}/config.json")
         self.api_base = configJson["api_base"]
@@ -32,13 +32,54 @@ class Config:
         self.bandori_station_token = configJson["bandori_station_token"]
         self.token_name = configJson["token_name"]
         self.api_use_proxy = bool(configJson["api_use_proxy"])
-        self.submit_car_number_use_proxy = bool(configJson["submit_car_number_use_proxy"])
+        self.submit_car_number_use_proxy = bool(
+            configJson["submit_car_number_use_proxy"])
         self.proxy_url = configJson["proxy_url"]
         self.server_list = configJson["server_list"]
         self.ban_gacha_simulate_group_data = configJson["ban_gacha_simulate_group_data"]
         self.enable_carNum_prompt_groups = configJson["enable_carNum_prompt_groups"]
         self.cmd_list = configJson["cmd_list"]
         self.car_config = configJson["car_config"]
+
+
+class User:
+    _id: str
+    user_id: str
+    platform: str
+    serverMode: int
+    defaultServer: list[int]
+    car: bool
+    server_list: list[dict]
+
+    def __init__(
+        self,
+        _id: str,
+        user_id: str,
+        platform: str,
+        serverMode: int,
+        defaultServer: list[int],
+        car: bool,
+        server_list: list[dict]
+    ):
+        """
+        Args:
+            _id (str): 唯一id
+            user_id (str): 用户ID
+            platform (str): 平台
+            serverMode (int): 服务器模式
+            defaultServer (list[int]): 默认服务器
+            car (str): 是否开启车牌转发
+            server_list (list[dict]): 服务器列表
+        """
+        self._id = _id
+        self.user_id = user_id
+        self.platform = platform
+        self.serverMode = serverMode
+        self.defaultServer = defaultServer
+        self.car = car
+        self.server_list = server_list
+
+
 
 def openJson(path) -> dict | None:
     try:
